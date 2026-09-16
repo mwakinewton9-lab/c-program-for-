9f84d0b6a203e80d3608b19fb7c2e931e5ca8bc3
mwakinewton9-lab <mwakinewton9@gmail.com>
2026-09-16 23:14:58 +0300

    Add files via upload

---
+++ c .program  (1).c
@@ -0,0 +1,18 @@
+//name: Newton mwaki
+//reg no:CT100/G/30742/26
+//Date:15/9/2026
+//course: information technology 
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("units offered this semester:\n");
+    printf ("introduction to programming\n ");
+    printf ("data structure\n ");
+    printf ("computer network\n ");
+    printf ("database system\n ");
+    printf ("software engineering\n ");
+    return 0; 
+}
